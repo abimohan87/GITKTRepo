7d6414a6b94b8edea2a3315b1eef8234b7114008
Abishek Mohan <abimohan87@gmail.com>
2022-01-04 15:11:51 +0530

    Onsite team added 2 test cases to BanchingDemo2.java file

--- src/test/java/end2endProject/BanchingDemo2.java
+++ src/test/java/end2endProject/BanchingDemo2.java
@@ -1,5 +1,16 @@
 package end2endProject;
 
+import org.testng.annotations.Test;
+
 public class BanchingDemo2 {
 
+	@Test
+	public void testCaseOnsite1() {
+		
+	}
+	@Test
+	public void testCaseOnsite2() {
+		
+	}
+	
 }
